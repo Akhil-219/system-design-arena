04b146713366174da78b60e8d6e47605673f2cf7
Akhil-219 <akhilkalipindi2@gmail.com>
2026-06-17 00:59:33 +0530

    feat: add upVote feature for the discussion post

---
+++ backend/src/services/discussionPostUpVote.service.js
@@ -0,0 +1,33 @@
+import { DiscussionPost } from "../models/discussion_posts.model.js";
+import { DiscussionPostVote } from "../models/discussion_post_votes.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const createUpVotePost=async({postId, userId})=>{
+    const post=await DiscussionPost.findById(postId)
+    if(!post){
+        throw new ApiError(404, "Post not found")
+    }
+    const existingVote= await DiscussionPostVote.findOne({postId:post,userId:userId})
+    if(existingVote){
+        throw new ApiError(400, "You have already liked the post")
+    }
+    await DiscussionPostVote.create({postId:post,userId:userId})
+    await DiscussionPost.findByIdAndUpdate(postId,{$inc:{upvoteCount:1}})
+    return true
+}
+
+const deleteUpVotePost=async({postId, userId})=>{
+    const post=await DiscussionPost.findById(postId)
+    if(!post){
+        throw new ApiError(404, "Post not found")
+    }
+    const existingVote= await DiscussionPostVote.findOne({postId:post,userId:userId})
+    if(!existingVote){
+        throw new ApiError(400, "Cant unlike the post again")
+    }
+    await DiscussionPostVote.findOneAndDelete({postId:post,userId:userId})
+    await DiscussionPost.findByIdAndUpdate(postId,{$inc:{upvoteCount:-1}})
+    return true;
+}
+
+export{createUpVotePost, deleteUpVotePost}
