0846efec35ce29bca903937f99a3a61f47169d1b
Akhil-219 <akhilkalipindi2@gmail.com>
2026-06-16 12:27:23 +0530

    feat: improve error handling and refactor upvote/comment services

--- backend/src/services/commDesignComment.service.js
+++ backend/src/services/commDesignComment.service.js
@@ -11,44 +11,46 @@ const createCommentInCommunity = async ({ designId, comment, userId }) => {
     throw new ApiError(400, "Cannot comment on unpublished design");
   }
   if (!comment?.trim()) {
-  throw new ApiError(400, "Comment cannot be empty");
-}
+    throw new ApiError(400, "Comment cannot be empty");
+  }
   const createdComment = await DesignComment.create({
     designId: designId,
     authorId: userId,
     content: comment,
   });
-  design.commentCount+=1;
-  await design.save({validateBeforeSave:false})
+  design.commentCount += 1;
+  await design.save({ validateBeforeSave: false });
   return { createdComment };
 };
 
-const getCommentsOfDesign =async({designId})=>{
-    const design= await Design.findById(designId)
-    if(!design){
-        throw new ApiError(404, "Design not found")
-    }
-    if(!design.isPosted){
-        throw new ApiError(400,"Cant access the comments of unpublished design")
-    }
-    const comments= await DesignComment.find({designId:design._id}).populate("authorId","username profilePicture reputation").sort({ createdAt: -1 })
-    return {comments}
-}
+const getCommentsOfDesign = async ({ designId }) => {
+  const design = await Design.findById(designId);
+  if (!design) {
+    throw new ApiError(404, "Design not found");
+  }
+  if (!design.isPosted) {
+    throw new ApiError(400, "Cant access the comments of unpublished design");
+  }
+  const comments = await DesignComment.find({ designId: design._id })
+    .populate("authorId", "username profilePicture reputation")
+    .sort({ createdAt: -1 });
+  return { comments };
+};
 
-const deleteComment= async({commentId,userId})=>{
-    const comment= await DesignComment.findById(commentId)
-    
-    if(!comment){
-        throw new ApiError(404,"Comment was already deleted")
-    }
-    if(comment.authorId.toString() !== userId.toString()){
-        throw new ApiError(403,"Cant delete another person's comment")
-    }
-    await DesignComment.findByIdAndDelete(commentId)
-    const design =await Design.findById(comment.designId)
-    design.commentCount-=1;
-    await design.save({ validateBeforeSave:false });
-    return true
-}
+const deleteComment = async ({ commentId, userId }) => {
+  const comment = await DesignComment.findById(commentId);
 
-export { createCommentInCommunity , deleteComment, getCommentsOfDesign};
+  if (!comment) {
+    throw new ApiError(404, "Comment was already deleted");
+  }
+  if (comment.authorId.toString() !== userId.toString()) {
+    throw new ApiError(403, "Cant delete another person's comment");
+  }
+  await DesignComment.findByIdAndDelete(commentId);
+  const design = await Design.findById(comment.designId);
+  design.commentCount -= 1;
+  await design.save({ validateBeforeSave: false });
+  return true;
+};
+
+export { createCommentInCommunity, deleteComment, getCommentsOfDesign };
--- backend/src/services/commDesignVote.service.js
+++ backend/src/services/commDesignVote.service.js
@@ -1,35 +1,52 @@
-import {Design} from "../models/design.model.js"
-import { DesignVote } from "../models/design_votes.model.js"
-import {ApiError} from "../utils/ApiError.js"
-const createUpVoteDesign =async({designId, userId })=>{
-    const design = await Design.findById(designId)
-    if(!design){
-        throw new ApiError(404,"Design not found")
-    }
-    if(!design.isPosted){
-        throw new ApiError(404, "Cant like the unposted design")
-    }
-    const upVote= await DesignVote.create({designId:designId, userId:userId})
-    design.upvoteCount+=1;
-    return true;
-}
+import { Design } from "../models/design.model.js";
+import { DesignVote } from "../models/design_votes.model.js";
+import { ApiError } from "../utils/ApiError.js";
+const createUpVoteDesign = async ({ designId, userId }) => {
+  const design = await Design.findById(designId);
+  if (!design) {
+    throw new ApiError(404, "Design not found");
+  }
+  if (!design.isPosted) {
+    throw new ApiError(400, "Cant like the unposted design");
+  }
+  const existingVote = await DesignVote.findOne({
+    designId,
+    userId,
+  });
 
-const deleteUpVoteDesign=async({designId,userId})=>{
-    const design= await Design.findById(designId)
-    if(!design){
-        throw new ApiError(404,"Design not found")
-    }
-    if(!design.isPosted){
-        throw new ApiError(404, "Cant unlike the unposted design")
-    }
-    const prevVoted = await DesignVote.findOne({designId:designId, userId:userId})
+  if (existingVote) {
+    throw new ApiError(400, "You have already upvoted this design");
+  }
+    await DesignVote.create({
+    designId: designId,
+    userId: userId,
+  });
+  await Design.findByIdAndUpdate(designId, { $inc: { upvoteCount: 1 } });
+  return true;
+};
 
-    if(!prevVoted){
-        throw new ApiError(404, "Cant unlike the design again")
-    }
-    await DesignVote.findOneAndDelete({designId:designId, userId:userId})
-    design.upvoteCount-=1;
-    return true
-}
+const deleteUpVoteDesign = async ({ designId, userId }) => {
+  const design = await Design.findById(designId);
+  if (!design) {
+    throw new ApiError(404, "Design not found");
+  }
+  if (!design.isPosted) {
+    throw new ApiError(404, "Cant unlike the unposted design");
+  }
+  const prevVoted = await DesignVote.findOne({
+    designId: designId,
+    userId: userId,
+  });
 
-export{createUpVoteDesign, deleteUpVoteDesign}
+  if (!prevVoted) {
+    throw new ApiError(400, "Cant unlike the design");
+  }
+  await DesignVote.findOneAndDelete({ designId: designId, userId: userId });
+  if (design.upvoteCount > 0) {
+    await Design.findByIdAndUpdate(designId, { $inc: { upvoteCount: -1 } });
+  }
+
+  return true;
+};
+
+export { createUpVoteDesign, deleteUpVoteDesign };
